a080aed84dc0368f83cba0b26975e212d70fa274
Padhu G <captchaking.padhu@gmail.com>
2016-07-22 19:19:48 +0000

    clean

--- acad.rb
+++ acad.rb
@@ -1,17 +1,17 @@
+# Implement of mixins
 module Num
   def no_type(number)
     if number.even?
-      puts "This is even number"
+      puts 'This is even number'
     else
-      puts "This is odd number"
+      puts 'This is odd number'
     end
   end
 end
 
+# A sample Class utilizing Num module as mixins
 class Test
   include Num
-  
-  
 end
 
 test = Test.new
